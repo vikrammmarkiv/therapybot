906b26b0f484d90b45af739aeabf9a9e8ac5fece
vikrammmarkiv <vikrammmarkiv@gmail.com>
2018-01-21 23:50:39 +0530

    Add files via upload

---
+++ bot/convoreply.js
@@ -0,0 +1,41 @@
+var responses = { // shift alt f
+    "sadness.first": [
+        "okay. So tell me why are you feeling sad!",		
+		"i care about you and i can't see you sad. Let's talk about this and try to overcome it.",
+		"I'm so glad you're here, share with me.",
+		"If you want to talk about your feeling, I'll listen.",
+		"It's all right not to feel ok."
+    ],
+	 "sadness.deg1": [
+        "Well, i want you to know that you are not alone in this. I'm always here for you.",
+		"Let me give you a hug!",
+		"I can understand what you are feeling",
+		 "I understand your pain and I empathize",
+		 "I know this is hard, but i will not give up on you.",
+		 "Thanks for sharing this with me. Do you want to tell me more about it?",
+		"you will not let these bad thoughts win.",
+		"Well, don't worry. Your success rate of surviving days like these so far is 100%",
+		"Do what you can do right now, and no more than that. That's all the universe expects of you."
+		
+		 
+	],
+	"sadness.deg2": [
+        "You are a strong person and when all this is over, I’ll still be here and so will you.",
+		"I’m sorry that you’re in so much pain, but we'll get through this.",
+		"All I want to do now is give you a hug and a shoulder to cry on..",
+		"I listen to you talk about it, and I can’t imagine what it’s like for you. I just can’t imagine how hard it must be.",
+		"Well, no matter how dark your days get, I'll be here for you. I'm just a click away.",
+		"I know you can get through this one",
+		"I'll be there to hug you when the sun comes up.",
+		"I can't even pretend to know what you're going through, but you aren't alone and you are cared for.",
+		"I will always have your back, my friend.",
+		"I know that the clouds must clear and the sun will shine",
+		"come on friend, you've got to get back up"
+		
+	],
+	"anger.deg1": [
+        "Don't let the darkness steal the beautiful person you have inside.",
+		
+    ]
+}
+module.exports = responses;
